e173552f83c660ebf6cdb18cc7959432e63de8ab
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-04-19 14:23:49 +0200

    добавлена команда /convert

--- exchange_commands.py
+++ exchange_commands.py
@@ -3,6 +3,7 @@ from aiogram import types
 import aiohttp
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ParseMode
+import json
 
 payload = {}
 headers= {
@@ -24,4 +25,33 @@ async def list_exchange_handler(message: types.Message):
     else:
         await message.reply(result)
 
+
+
+async def convert_convert_handler(message: types.Message):
+    #/convert to from amount
+    try:
+        to = message.text.split()[1]
+        fro=message.text.split()[2]
+        amount=message.text.split()[3]
+    except Exception as e:
+        print("Exception (find):", e)
+        await message.reply("Введите /convert СкакойВалюты ВКаккуюВалюту Кол-воВалюты")
+        await message.reply("Если не знаете как обозначить валюты /list_ex")
+        return
+
+    try:
+        url = "https://api.apilayer.com/currency_data/convert?to={}&from={}&amount={}".format(to, fro, amount)
+
+    except Exception as e:
+        print("Exception (find):", e)
+        await message.reply("Exception (find):", e)
+        return
+    response = requests.request("GET", url, headers=headers, data=payload)
+    status_code = response.status_code
+    result = response.text
+    print(
+        f"Из {json.loads(result)['query']['amount']} {json.loads(result)['query']['from']} мы получили {json.loads(result)['result']} {json.loads(result)['query']['to']}")
+
+    await message.reply(
+        f"Из {json.loads(result)['query']['amount']} {json.loads(result)['query']['from']} мы получили {json.loads(result)['result']} {json.loads(result)['query']['to']}")
 
