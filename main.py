3452ff63d8ec03609bc25b467864ec28ca2bcd42
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-04-19 17:04:27 +0200

    добавил обработчик

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import logging
 import asyncio
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher import dispatcher
+from aiogram.types import ParseMode
 from aiogram.utils import executor
 
 from photo_commands import animal_handler
+from poll_commands import create_poll_handler
+
 from start_commands import start_handler
 from other_commands import help_handler, echo_handler
 from weather_caommand import weather_handler
@@ -14,29 +18,27 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token='5343231561:AAGB0nKpggD61U7t83sNgW_a0baKCQk2Deo')
 dp = Dispatcher(bot)
+
+dp.register_message_handler(create_poll_handler, commands=["createpoll"])
 dp.register_message_handler(animal_handler, commands=['animal'])
 dp.register_message_handler(convert_convert_handler, commands=['convert'])
 dp.register_message_handler(list_exchange_handler, commands=['list_ex'])
 dp.register_message_handler(weather_handler, commands=['weather'])
 dp.register_message_handler(start_handler, commands=['start'])
 dp.register_message_handler(help_handler, commands=['help'])
-dp.register_message_handler(echo_handler)
-
+# dp.register_message_handler(echo_handler)
 
 async def on_startup(dp):
     await bot.send_message(chat_id='556907227', text='Бот запущен')
 
 
 async def on_shutdown(dp):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
     await bot.send_message(chat_id='556907227', text='Бот остановлен')
 
 
 async def main():
-    # Регистрация обработчиков команд
-    dp.register_message_handler(start_handler, commands=['start'])
-    dp.register_message_handler(help_handler, commands=['help'])
-    dp.register_message_handler(echo_handler)
-
     # Запуск бота
     await dp.start_polling(on_startup=on_startup, on_shutdown=on_shutdown)
 
