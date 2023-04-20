ec82576b0f2f9d4baf73b426432a5eea271869dc
Valiantsin Bychynski <bychinskiy.valentin@mail.ru>
2023-04-20 11:07:47 +0200

    Написаны описания команд

--- other_commands.py
+++ other_commands.py
@@ -2,4 +2,12 @@ from aiogram import types
 
 
 async def help_handler(message: types.Message):
-    await message.reply("Я могу понимать команды /start и /help")
+    reply_text = "Привет! Я могу выполнить следующие команды:\n"
+    reply_text += "/start - запустить бота и получить приветственное сообщение\n"
+    reply_text += "/help - получить справочную информацию о командах бота\n"
+    reply_text += "/animal - получить случайное фото животного\n"
+    reply_text += "/convert - конвертировать валюту (например: /convert USD EUR 100)\n"
+    reply_text += "/list_ex - получить список доступных валют для конвертации\n"
+    reply_text += "/weather - получить прогноз погоды для указанного города (например: /weather Moscow)"
+
+    await message.reply(reply_text)
--- start_commands.py
+++ start_commands.py
@@ -1,5 +1,7 @@
 from aiogram import types
-
+from buttons import *
 
 async def start_handler(message: types.Message):
+    # Отправка сообщения с клавиатурой
+    await message.answer("Выберите команду:", reply_markup=keyboard)
     await message.reply("Привет!")
